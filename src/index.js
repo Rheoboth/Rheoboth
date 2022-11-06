2bab54c1da529410dc6cc71bee09ed4167ec808d
Rehoboth <108800247+Rheoboth@users.noreply.github.com>
2022-11-06 23:06:48 +0100

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import createRoot from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
+import { AuthProvider } from "./AuthContext";
 
 // Wrap our App in BrowserRouter => in the React Router context....
+// const Context = React.createContext(defaultValue);
 createRoot.render(
  <BrowserRouter>
    <React.StrictMode>
-     <App />
+   <AuthProvider>
+		  <App />
+	  </AuthProvider>
    </React.StrictMode>
  </BrowserRouter>,
   document.getElementById('root')
